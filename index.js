7d3e58fa2ea2089f3ea72d7dde8e9f6b34acde32
Jacky Tang <jackytck@gmail.com>
2017-06-12 00:21:44 +0800

    :hankey: add 'New Todo' menu

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electron = require('electron')
 const {
   app,
   BrowserWindow,
+  Menu
 } = electron
 
 let mainWindow
@@ -10,4 +11,16 @@ let mainWindow
 app.on('ready', () => {
   mainWindow = new BrowserWindow({})
   mainWindow.loadURL(`file://${__dirname}/main.html`)
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(mainMenu)
 })
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      { label: 'New Todo' }
+    ]
+  }
+]
