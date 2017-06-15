429197d9c286f7efc2803bdf5e11a7ca9c0c3d8f
Jacky Tang <jackytck@gmail.com>
2017-06-16 00:03:31 +0800

    :sparkles: send todo from add.html to index.js to main.html

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electron = require('electron')
 const {
   app,
   BrowserWindow,
+  ipcMain,
   Menu
 } = electron
 
@@ -25,7 +26,13 @@ function createAddWindow () {
     title: 'Add New Todo'
   })
   addWindow.loadURL(`file://${__dirname}/add.html`)
+  addWindow.on('closed', () => addWindow = null)
 }
+
+ipcMain.on('todo:add', (event, todo) => {
+  mainWindow.webContents.send('todo:add', todo)
+  addWindow.close()
+})
 
 const menuTemplate = [
   {
@@ -57,6 +64,7 @@ if (process.env.NODE_ENV !== 'production') {
   menuTemplate.push({
     label: 'View',
     submenu: [
+      { role: 'reload' },
       {
         label: 'Toggle Devleoper Tools',
         accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
